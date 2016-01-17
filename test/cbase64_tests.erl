d70e8762ab8b7ea26ed90c5af6acb9c7ed904a14
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2016-01-18 01:30:50 +0300

    Turn tabs into spaces

--- test/cbase64_tests.erl
+++ test/cbase64_tests.erl
@@ -4,33 +4,31 @@
 -include_lib("eunit/include/eunit.hrl").
 
 empty_test() ->
-	?assertEqual(
-		<<>>,
-		cbase64:decode( cbase64:encode(<<>>) )
-	).
+    ?assertEqual(<<>>, cbase64:encode(<<>>)),
+    ?assertEqual(<<>>, cbase64:decode(<<>>)).
 
 proper_small_test_() ->
-	PropErOpts = [
-		{to_file, user},
-		{max_size, 64},
-		{numtests, 10240}
-	],
-	{timeout, 60, ?_assertEqual([], proper:module(?MODULE, PropErOpts))}.
+    PropErOpts = [
+        {to_file, user},
+        {max_size, 64},
+        {numtests, 10240}
+    ],
+    {timeout, 60, ?_assertEqual([], proper:module(?MODULE, PropErOpts))}.
 
 proper_big_test_() ->
-	PropErOpts = [
-		{to_file, user},
-		{max_size, 1048576},
-		{numtests, 64}
-	],
-	{timeout, 3600, ?_assertEqual([], proper:module(?MODULE, PropErOpts))}.
+    PropErOpts = [
+        {to_file, user},
+        {max_size, 1048576},
+        {numtests, 64}
+    ],
+    {timeout, 3600, ?_assertEqual([], proper:module(?MODULE, PropErOpts))}.
 
 prop_cbase64_enc_dec() ->
-	?FORALL(Data, binary(),
-		begin
-			EncData = cbase64:encode(Data),
-			EncData =  base64:encode(Data),
-			Data = cbase64:decode(EncData),
-			Data =  base64:decode(EncData),
-			true
-		end).
+    ?FORALL(Data, binary(),
+        begin
+            EncData = cbase64:encode(Data),
+            EncData = base64:encode(Data),
+            Data = cbase64:decode(EncData),
+            Data = base64:decode(EncData),
+            true
+        end).
